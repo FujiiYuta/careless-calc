348f93b819dd18cfc804fa6deab7e25dd2cdb454
Yuta Air <yutahand1@yahoo.co.jp>
2025-01-22 23:52:38 +0900

    追従できていなかったファイルを追加

--- CarelessCalc/CalculatorBrain.swift
+++ CarelessCalc/CalculatorBrain.swift
@@ -2,32 +2,41 @@
 //  CalculatorBrain.swift
 //  Calculator
 //
-//  Created by Andrea Vultaggio on 17/10/2017.
-//  Copyright © 2017 Andrea Vultaggio. All rights reserved.
+//  Created by Yuta Fujii on 22/01/2025.
+//  Copyright © 2025 Yuta Fujii. All rights reserved.
 //
 
 import Foundation
+import AVFoundation
 
 struct CalculatorBrain {
-    
+
     //MARK: Variables
-    
+
     private var accumulator: Double?
     private var pendingBinaryOperation: PendingBinaryOperation?
     private var resultIsPending = false
-    
+    private var player: AVAudioPlayer?
+
     var description = ""
     var result: Double? { get { return accumulator } }
-    
+    var alertType: AlertType = .correct
+
+    enum AlertType {
+        case mistake
+        case _2143
+        case correct
+    }
+
     //MARK: Enumerations
-    
+
     private enum Operation {
         case constant(Double)
         case unaryOperation((Double) -> Double)
         case binaryOperation((Double, Double) -> Double)
         case result
     }
-    
+
     private var operations: Dictionary<String, Operation> = [
         "＋" : .binaryOperation({ $0 + $1 }),
         "﹣" : .binaryOperation({ $0 - $1 }),
@@ -39,21 +48,22 @@ struct CalculatorBrain {
          "AC": .constant(0),
          "=" : .result
     ]
-    
+
     //MARK: Embedded struct
 
     private struct PendingBinaryOperation {
         let function: (Double, Double) -> Double
         let firstOperand: Double
-        
-        func perform(with secondOperand: Double) -> Double {
-            let baseResult = function(firstOperand, secondOperand) // 通常の演算結果を計算
+        var hasMistake = false
 
+        mutating func perform(with secondOperand: Double) -> Double {
+            let baseResult = function(firstOperand, secondOperand) // 通常の演算結果を計算
             // 30%の確率でエラーを発生させる
             if Bool.random(probability: 0.3) {
+                self.hasMistake = true
                 return introduceError(to: baseResult) // 結果にエラーを導入
             }
-
+            self.hasMistake = false
             return baseResult // 通常の演算結果を返す
         }
 
@@ -67,23 +77,31 @@ struct CalculatorBrain {
             return value + Double(missValue)
         }
     }
-    
+
     //MARK: Functions
-    
+
     private mutating func performPendingBinaryOperation() {
         if pendingBinaryOperation != nil && accumulator != nil {
             accumulator = pendingBinaryOperation?.perform(with: accumulator!)
+            if accumulator == 2143 {
+                alertType = ._2143
+            } else if pendingBinaryOperation?.hasMistake ?? false {
+                alertType = .mistake
+            } else {
+                alertType = .correct
+            }
             pendingBinaryOperation = nil
             resultIsPending = false
         }
     }
-    
+
     mutating func performOperation(_ symbol: String) {
         if let operation = operations[symbol] {
             switch operation {
                 case .constant(let value):
                     accumulator = value
                     description = ""
+                    alertType = .correct
                 case .unaryOperation(let function):
                     if accumulator != nil {
                         let value = String(describing: accumulator!).removeAfterPointIfZero()
@@ -92,28 +110,28 @@ struct CalculatorBrain {
                     }
                 case .binaryOperation(let function):
                     performPendingBinaryOperation()
-                    
+
                     if accumulator != nil {
                         if description.last == "=" {
                             description = String(describing: accumulator!).removeAfterPointIfZero().setMaxLength(of: 5) + symbol
                         } else {
                             description += symbol
                         }
-                        
+
                         pendingBinaryOperation = PendingBinaryOperation(function: function, firstOperand: accumulator!)
                         resultIsPending = true
                         accumulator = nil
                     }
                 case .result:
                     performPendingBinaryOperation()
-                    
+
                     if !resultIsPending {
                         description += "="
                     }
             }
         }
     }
-    
+
     mutating func setOperand(_ operand: Double?) {
         accumulator = operand ?? 0.0
         if !resultIsPending {
@@ -122,4 +140,5 @@ struct CalculatorBrain {
             description += String(describing: operand!).removeAfterPointIfZero().setMaxLength(of: 5)
         }
     }
+
 }
--- CarelessCalc/ViewController.swift
+++ CarelessCalc/ViewController.swift
@@ -2,30 +2,33 @@
 //  ViewController.swift
 //  Calculator
 //
-//  Created by Andrea Vultaggio on 17/10/2017.
-//  Copyright © 2017 Andrea Vultaggio. All rights reserved.
+//  Created by Yuta Fujii on 22/01/2025.
+//  Copyright © 2025 Yuta Fujii. All rights reserved.
 //
 
 import UIKit
 import DeviceKit
+import AVFoundation
 
-class ViewController: UIViewController {
-    
+class ViewController: UIViewController, AVAudioPlayerDelegate {
+
     //MARK: Outlets
-    
+
     @IBOutlet weak var sequence: UILabel!
     @IBOutlet weak var cornerView: UIView!
     @IBOutlet weak var display: UILabel!
-    
+
     //MARK: Variables
-    
+
     private var brain = CalculatorBrain()
     private var userIsInTheMiddleOfTyping = false
-    
+
+    private var player: AVAudioPlayer?
+
     private var iPhoneModel: Device {
         return Device.realDevice(from: .current)
     }
-    
+
     private var displayValue: Double {
         get {
             return Double(display.text ?? Constants.emptyString) ?? Double.nan
@@ -35,40 +38,40 @@ class ViewController: UIViewController {
             display.text = tmp.setMaxLength(of: 8)
         }
     }
- 
+
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
-    
+
     override var prefersStatusBarHidden: Bool {
         return false
     }
-    
+
     //MARK: UIVIew Delegate
-    
+
     override func viewDidLoad() {
         // round the corners of the calculator on iPhones with the notch.
         if Device.allDevicesWithSensorHousing.contains(iPhoneModel) {
             cornerView.layer.cornerRadius = Constants.cornerRadius
             cornerView.layer.masksToBounds = true
-        } 
+        }
     }
-    
+
     //MARK: IBAction(s)
-    
+
     @IBAction func touchDigit(_ sender: UIButton) {
         guard let digit = sender.currentTitle else { return }
-        
+
         if userIsInTheMiddleOfTyping {
             guard let textCurrentlyInDisplay = display.text else { return }
-            
+
             if digit == "." && (textCurrentlyInDisplay.range(of: Constants.decimalPoint) != nil) {
                 return
             } else {
                 let tmp = textCurrentlyInDisplay + digit
                 display.text = tmp.setMaxLength(of: Constants.maxStringLength)
             }
-            
+
         } else {
             if digit == Constants.decimalPoint {
                 display.text = Constants.pointAfterZero
@@ -77,26 +80,55 @@ class ViewController: UIViewController {
             }
             userIsInTheMiddleOfTyping = true
         }
-        
+
         sequence.text = brain.description
     }
-    
+
     @IBAction func performOperation(_ sender: UIButton) {
         if userIsInTheMiddleOfTyping {
             brain.setOperand(displayValue)
             userIsInTheMiddleOfTyping = false
         }
-        
+
         if let mathematicalSymbol = sender.currentTitle {
             brain.performOperation(mathematicalSymbol)
         }
-        
+
         if let result = brain.result {
             displayValue = result
+            switch brain.alertType {
+            case ._2143:
+                playErrorSound(name: "2143")
+            case .mistake:
+                playErrorSound(name: "stupid1")
+            case .correct:
+                break
+            }
         }
-        
+
         sequence.text = brain.description
     }
+
+    /// エラー音声を再生する
+    private func playErrorSound(name: String) {
+        guard let path = Bundle.main.path(forResource: name, ofType: "m4a") else {
+            print("音源ファイルが見つかりません")
+            return
+        }
+
+        do {
+            // AVAudioPlayerのインスタンス化
+            player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: path))
+
+            // AVAudioPlayerのデリゲートをセット
+            player?.delegate = self
+
+            // 音声の再生
+            player?.play()
+        } catch {
+        }
+    }
+
 }
 
 extension ViewController {
